c9e610bad591e832ac7976e855d365b0943831f0
Paul A. Tumwine <paulatumwine@gmail.com>
2020-09-03 00:15:16 -0500

    Try out an update to table in HBase

--- hbase/src/main/java/hbaseTest/HbaseTable.java
+++ hbase/src/main/java/hbaseTest/HbaseTable.java
@@ -18,8 +18,9 @@ public class HbaseTable
 		Configuration config = HBaseConfiguration.create();
 
 		try (Connection connection = ConnectionFactory.createConnection(config);
-				Admin admin = connection.getAdmin())
-		{
+                Admin admin = connection.getAdmin();
+                Table table1 = connection.getTable(TableName.valueOf(TABLE_NAME))
+        ) {
 			HTableDescriptor table = new HTableDescriptor(TableName.valueOf(TABLE_NAME));
 			table.addFamily(new HColumnDescriptor("Row Key").setCompressionType(Algorithm.NONE));
 			table.addFamily(new HColumnDescriptor("Personal Data"));
@@ -36,15 +37,16 @@ public class HbaseTable
 
             System.out.print("Inserting data... ");
 
-//            HTable table1 = new HTable(config, TABLE_NAME);
-            Table table1 = connection.getTable(TableName.valueOf(TABLE_NAME));
-
             table1.put(getPut("row1", "1", "John", "Boston", "Manager", "150,000"));
             table1.put(getPut("row2", "2", "Mary", "New York", "Sr. Engineer", "130,000"));
             table1.put(getPut("row3", "3", "Bob", "Fremont", "Jr. Engineer", "90,000"));
             System.out.println("data inserted successfully");
 
-            table1.close();
+            System.out.print("Updating some data... ");
+            debug(table1);
+
+            table1.put(getPut("row3", "3", "Bob", "Fremont", "Sr. Engineer", Double.toString(90000*1.05)));
+            debug(table1);
 
 			System.out.println(" Done!");
 		} catch (Exception e) {
@@ -62,5 +64,15 @@ public class HbaseTable
         p.addColumn(Bytes.toBytes("Professional Data"), Bytes.toBytes("salary"), Bytes.toBytes(sal));
 
         return p;
+    }
+
+    private static void debug(Table table1) throws IOException {
+        Get g = new Get(Bytes.toBytes("row3"));
+        Result r = table1.get(g);
+        byte [] value = r.getValue(Bytes.toBytes("Professional Data"),Bytes.toBytes("Designation"));
+        byte [] value1 = r.getValue(Bytes.toBytes("Professional Data"),Bytes.toBytes("salary"));
+        String des = Bytes.toString(value);
+        String sal = Bytes.toString(value1);
+        System.out.println(des + " " + sal);
     }
 }
